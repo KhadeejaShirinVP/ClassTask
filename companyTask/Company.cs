e18e942146759aceadad4ad914c2f20ee6cf7744
KhadeejaShirinVP <khadeejasvp@moback.com>
2022-09-29 17:18:10 +0100

    Class

---
+++ companyTask/Company.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace companyTask
+{
+    class Company
+    {
+        private int id;
+        private string name;
+        private string location;
+        private int numberOfEmploy;
+        private int numberOfManagers;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public int NumberOfEmploy
+        {
+            get { return numberOfEmploy; }
+            set { numberOfEmploy = value; }
+        }
+        public int NumberOfManagers
+        {
+            get { return numberOfManagers; }
+            set { numberOfManagers = value; }
+        }
+
+        public void TotalEmplyees()
+        {
+            Console.WriteLine("Total number of employees\t" + (numberOfEmploy + numberOfManagers));
+        }
+
+    }
+}
---
+++ companyTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace companyTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First  compnay details");
+            Company company1 = new Company();
+            company1.Id = 1;
+            company1.Name = "moback";
+            company1.Location = "banglure";
+            company1.NumberOfEmploy = 210;
+            company1.NumberOfManagers = 12;
+            company1.TotalEmplyees();
+
+            Console.WriteLine("Company id-\t" + company1.Id);
+            Console.WriteLine("Company name-\t" + company1.Name);
+            Console.WriteLine("Copmany location-\t" + company1.Location);
+            Console.WriteLine("number of employees-\t" + company1.NumberOfEmploy);
+            Console.WriteLine("number of managers-\t" + company1.NumberOfManagers);
+            
+
+            Console.WriteLine("Second company details");
+            Company company2= new Company();
+            company2.Id=2;
+            company2.Name = "uber";
+            company2.Location = "kochi";
+            company2.NumberOfEmploy = 500;
+            company2.NumberOfManagers = 87;
+            company2.TotalEmplyees();
+
+            Console.WriteLine("Company id-\t" + company2.Id);
+            Console.WriteLine("Company name-\t" + company2.Name);
+            Console.WriteLine("Company location-\t"+ company2.Location);
+            Console.WriteLine("Company employees-\t" + company2.NumberOfEmploy);
+            Console.WriteLine("Company managers-\t"+ company2.NumberOfManagers);
+
+            Console.ReadKey();
+          
+        }
+    }
+}
